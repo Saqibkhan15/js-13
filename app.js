1acfa07d69dfbbedd6428302df78e7c421495e98
Saqibkhan15 <aqib12u@gmail.com>
2024-10-17 10:09:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+//assignment
+1; //console.log(new Date());
+//2
+//var month = new Date();
+// var monthNames = ["january","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
+// console.log(monthNames[month.getMonth()])
+
+// var days = new Date();
+// var today = days.getDay()
+// if (today === 4) {
+// alert("today is thursday !");
+// }
+
+// var days = new Date();
+// var today = days.getDay()
+
+// if (today === 4) {
+// alert("Today is Thursday!");
+// }
+
+// if (today === 0 || today === 6) {
+// alert("Fun Day!");
+// }
+// var date = new Date();
+// var dayOfMonth = date.getDate();
+
+// if (dayOfMonth < 16) {
+// alert("First fifteen days of the month");
+// } else {
+// alert("Last days of the month");
+// }
+
+// var now = new Date();
+// var hour = now.getHours();
+// if (hour < 12) {
+// alert("It's AM");
+// } else {
+// alert("It's PM");
+// }
+
+// var laterDate = new Date('December 31, 2020');
+// console.log(laterDate)
+
+// var ramadanStart = new Date('March 22, 2024');
+// var daysPassed = (new Date() - ramadanStart) / (1000 * 60 * 60 * 24);
+// alert(Math.floor(daysPassed) + " days have passed since Ramadan started.")
+
+
+// var referenceDate = new Date('January 1, 1970')
+// var targetDate = new Date('January 1, 2015')
+// var millisecondsDiff = targetDate.getTime() - referenceDate.getTime()
+// var secondsElapsed = millisecondsDiff / 1000
+// document.write("Seconds elapsed: " + secondsElapsed)
+
+
+// var currentTime = new Date();
+// var hours = currentTime.getHours();
+// document.write("Current Hours: " + hours + "<br>");
+// currentTime.setHours(currentTime.getHours() + 1);
+// document.write("Updated Date and Time: " + currentTime);
+
+// var currentDate = new Date();
+// currentDate.setFullYear(currentDate.getFullYear() - 100);
+// alert("Date 100 years back: " + currentDate.toLocaleDateString());
+
+// var age = parseInt(prompt("Enter your age:"));
+// var currentYear = new Date().getFullYear();
+// var birthYear = currentYear - age;
+// document.write("Your Age: " + age + "");
+// document.write("Your Birth Year: " + birthYear);
